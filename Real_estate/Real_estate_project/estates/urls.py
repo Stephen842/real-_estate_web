74ea8013ce38753802424990e6162ad709f77f07
stephen842 <ugotachisomstephen@gmail.com>
2024-02-10 22:45:26 +0100

    created all required html templates

--- Real_estate/Real_estate_project/estates/urls.py
+++ Real_estate/Real_estate_project/estates/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include 
 from django.conf import settings
 from .import views
-from django.conf.url.static import static
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
--- Real_estate/Real_estate_project/estates/views.py
+++ Real_estate/Real_estate_project/estates/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+
+#this part below is importations of modules that will be used for the authentication aspect of this project
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'pages/home.html')
