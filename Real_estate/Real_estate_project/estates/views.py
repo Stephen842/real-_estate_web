c5420233d1dffc6cb318d859dd3c92a1af8c00bc
Stephen842 <ugotachisomstephen@gmail.com>
2024-02-12 09:48:01 +0100

    still working

--- Real_estate/Real_estate_project/estates/views.py
+++ Real_estate/Real_estate_project/estates/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from datetime import datetime
 #this part below is importations of modules that will be used for the authentication aspect of this project
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def home(request):
-    return render(request, 'pages/master.html')
+    current_datetime = datetime.now()
+
+    context = {
+        'date': current_datetime,
+    }
+    return render(request, 'pages/master.html', context)
