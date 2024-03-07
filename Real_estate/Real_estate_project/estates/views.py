5e7de3c5c9b555c167d3128659b42aae8d995f2c
Stephen842 <ugotachisomstephen@gmail.com>
2024-03-07 10:58:35 +0100

    working on the blog aspect of this project

--- Real_estate/Real_estate_project/estates/views.py
+++ Real_estate/Real_estate_project/estates/views.py
@@ -3,9 +3,13 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.template import loader
 from datetime import datetime
+
 #this part below is importations of modules that will be used for the authentication aspect of this project
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+
+#this part is for the blog aspect of this project
+from .models import Post, Comment
 
 # Create your views here.
 def home(request):
@@ -22,3 +26,31 @@ def about(request):
         'title': 'Discover Platform Estates: Your Premier Real Estate Partner',
     }
     return render(request, 'pages/about.html', context)
+
+
+def blog_list(request):
+    posts = Post.objects.all().order_by('-date_created')
+
+    context = {
+                'posts': posts,
+            }
+    return render(request, 'pages/blog.html', context)
+
+def blog_detail(request, pk):
+    posts = Post.objects.get(pk=pk)
+    comments = Comment.objects.filter(post=post)
+
+    context = {
+            'posts': posts,
+            'comments': comments,
+            }
+    return render(request, 'pages/blog_detail.html', context)
+
+def blog_category(request, category):
+    posts = Post.objects.filter(categories__name__contains=category).order_by('-date_created')
+
+    context = {
+            'category': category,
+            'posts': posts,
+            }
+    return render(request, 'pages/blog_category.html', context)
