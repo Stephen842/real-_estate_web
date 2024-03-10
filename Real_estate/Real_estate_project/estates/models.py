892d658be9a5077133d704eed13e89791a6d0f72
Stephen842 <ugotachisomstephen@gmail.com>
2024-03-10 07:18:35 +0100

    hotfix

--- Real_estate/Real_estate_project/estates/models.py
+++ Real_estate/Real_estate_project/estates/models.py
@@ -14,6 +14,7 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length = 300)
     body = models.TextField()
+    slug = models.SlugField(max_length = 300, unique = True)
     date_created = models.DateTimeField(auto_now_add = True)
     categories = models.ManyToManyField('Category', related_name = 'posts')
 
