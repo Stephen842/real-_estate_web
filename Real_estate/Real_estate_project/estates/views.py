fca76dad72e26dd7c622b500ae8946c913fa4ee8
Stephen842 <ugotachisomstephen@gmail.com>
2024-03-12 14:52:16 +0100

    working on integrating the blog to the home and about page

--- Real_estate/Real_estate_project/estates/views.py
+++ Real_estate/Real_estate_project/estates/views.py
@@ -14,44 +14,57 @@ from .models import Post, Comment
 # Create your views here.
 def home(request):
     current_datetime = datetime.now()
+    posts = Post.objects.all().order_by('-date_created')
 
     context = {
         'date': current_datetime,
         'title': 'Your Real Estates Developer/ Consultant and Facility Manager',
+        'posts': posts,
     }
     return render(request, 'pages/home.html', context)
 
 def about(request):
+    current_datetime = datetime.now()
+    posts = Post.objects.all().order_by('-date_created')
+    
     context = {
         'title': 'Discover Platform Estates: Your Premier Real Estate Partner',
+        'posts': posts,
+        'date': current_datetime,
     }
     return render(request, 'pages/about.html', context)
 
 
 def blog_list(request):
+    current_datetime = datetime.now()
     posts = Post.objects.all().order_by('-date_created')
 
     context = {
                 'posts': posts,
-                'title': 'Property Insights: Expert Tips, Trends, and Stories'
+                'title': 'Property Insights: Expert Tips, Trends, and Stories',
+                'date': current_datetime,
             }
     return render(request, 'pages/blog.html', context)
 
 def blog_detail(request, pk):
+    current_datetime = datetime.now()
     post = Post.objects.get(pk=pk)
     comments = Comment.objects.filter(post=post)
 
     context = {
             'post': post,
             'comments': comments,
+            'date': current_datetime,
             }
     return render(request, 'pages/blog_detail.html', context)
 
 def blog_category(request, category):
     posts = Post.objects.filter(categories__name__contains=category).order_by('-date_created')
-
+    current_datetime = datetime.now()
+    
     context = {
             'category': category,
             'posts': posts,
+            'date': current_datetime,
             }
     return render(request, 'pages/blog_category.html', context)
