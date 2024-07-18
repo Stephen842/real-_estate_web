c854c9920cf9fef2384046ab4c4dbbce8dff5428
Stephen842 <ugotachisomstephen@gmail.com>
2024-07-18 20:28:40 +0100

    hotfix

--- Real_estate/Real_estate_project/tailwind.config.js
+++ Real_estate/Real_estate_project/tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
       colors: {
         'light-grey': '#cccfd1',
         'coral-pink': '#ff7474',
+        'darkBlue': '#1E3A8A',
+        'darkRed': '#8B0000',
       },
       fontFamily: {
         'magnolia': ['Magnolia Script', 'cursive'],
